d4475642b53ccbbc316c701054645d1fa5910f1a
joe santos garcia <joesantosgarcia@gmail.com>
2020-07-16 14:44:06 -0400

    added new changes to package.json

--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,120 @@
 import Head from 'next/head'
+import Layout from '../components/Layout';
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+import moment from 'moment';
+import Link from "next/link";
 
-export default function Home() {
+
+export default function Home({posts}) {
   return (
-    <div className="container">
+    <Layout>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Joe's Blog</title>
       </Head>
-
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
+      {/* Banner */}
+      <section id="banner" className="major">
+        <div className="inner">
+          <header className="major">
+            <h1>Hi, my name is Forty</h1>
+          </header>
+          <div className="content">
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              A responsive site template designed by HTML5 UP
+              <br />
+              and released under the Creative Commons.
             </p>
-          </a>
+            <ul className="actions">
+              <li>
+                <a href="#one" className="button next scrolly">
+                  Get Started
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-      </main>
+      </section>
+      {/* Main */}
+      <div id="main">
+        {/* One */}
+        <section id="one" className="tiles">
+          {/* Loop over posts */}
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  )
+          {posts.map((post) => (
+            <article>
+              <span className="image">
+                <img src={`/assets/images/${post.featured_image}`} alt="" />
+              </span>
+              <header className="major">
+                <h3>
+                  <Link href={`/${post.slug}`} className="link">
+                    {post.title}
+                  </Link>
+                </h3>
+                {/* <p>Ipsum dolor sit amet</p> */}
+              </header>
+            </article>
+          ))}
+        </section>
+        {/* Two */}
+        <section id="two">
+          <div className="inner">
+            <header className="major">
+              <h2>Massa libero</h2>
+            </header>
+            <p>
+              Nullam et orci eu lorem consequat tincidunt vivamus et sagittis
+              libero. Mauris aliquet magna magna sed nunc rhoncus pharetra.
+              Pellentesque condimentum sem. In efficitur ligula tate urna.
+              Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor.
+              Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis
+              libero. Mauris aliquet magna magna sed nunc rhoncus amet pharetra
+              et feugiat tempus.
+            </p>
+            <ul className="actions">
+              <li>
+                <a href="landing.html" className="button next">
+                  Get Started
+                </a>
+              </li>
+            </ul>
+          </div>
+        </section>
+      </div>
+    </Layout>
+  );
 }
+
+
+
+export const getStaticProps = async () => {
+  
+  const sortPosts = () => {
+    const allPosts = fs.readdirSync("posts").map((filename) => {
+      const file = fs.readFileSync(path.join("posts", filename)).toString();
+      
+      const postData = matter(file);
+      // console.log(postData.data.slug)
+      return {
+        content: postData.content,
+        title: postData.data.title,
+        featured_image: postData.data.featured_image,
+        date: postData.data.date,
+        slug: postData.data.slug
+      };
+      // console.log(`name: ${data.data.date}`);
+    });
+    
+    
+    return allPosts.sort((a,b) => new moment(a.date).format('YYYY-MM-DD HH:mm:ss') < new moment(b.date).format('YYYY-MM-DD HH:mm:ss'))
+    
+  }
+  console.log(sortPosts())
+
+  return {
+    props: {
+      posts: sortPosts(),
+    },
+  };
+};
---
+++ posts/top-javascript-frameworks.md
@@ -0,0 +1,44 @@
+---
+title: Top Javascript Frameworks
+slug: top-javascript-frameworks
+featured_image: top-javascript-frameworks.jpg
+date: "2020-07-03 12:33:23"
+---
+
+## This is a blog post about javascript frameworks
+
+Lorem markdownum utque, quem motibus difficilis undis. Latebramque tactas
+viribus iussa, umbrosa Tydides ipsa et valle arbusta Piscique media clamantia
+quaque non neve Elis. Experientis novas revocataque moram tot, est dum primum
+quoque puerilem, tua ferebat iram, totidem. Absens sis ista unda Iunonia stabant
+manibus vastat **luctus**, fatidicamque pastae animi detrusum. Oreada est ore
+defixa subduxit odiumque profecisse, supremaque a quisque raptae.
+
+Preces qui. Movere Thessala erit celeberrimus, vidit [simul
+Calydonia](http://fluidos.com/multos) at quaerit esse, *utque Telamon*. Est
+miseri est rutilis collum strepitum Aegeus in cupit tua.
+
+Plantis turres qua ministri, e cognita, ministri aper exsiliantque, moliturque.
+Si mittat, vix luctus ferenti, tum eodem sua, profundo at recingitur coepta?
+
+## Tepidique corpora virgo
+
+Sacrasque pignora, non luctus preces genitas. Licet videre quam audax subtexere
+vulnere cum fecit positae pia hoc monitis mora. Recisum herba relinquar, labens,
+semper conceditur sensit in requirere innumeras orbe.
+
+[Vobis](http://visceribus-furta.com/lyciamque.html) plus, illo salices aurea
+thalamique cervix furores, forte Helicen, bracchia est corruat turris rigorem
+orantem. Paludes quod Liber totum Arctos levis obliquo lucentis declive postquam
+mota. Molles dicebant poscitur iuvenem utrumque inter defendere tonitrua navit
+sensit Harpalos haeserunt. Petentia arce nec dolos inguine latentia et senes
+feliciter vident, *pictis sorores non* videri.
+
+> Opaca aestuat vos Athamas poenas. Merito unda anni **esset dea** virumque dies
+> angustum facies iurares!
+
+Tamen est umida differtis adclivi crede Saturnia viro rebus agmen ignavi
+aeacidae nemorosis. Agitatis e currus Argosque folio, tibi brevi multis iugulum
+discedens temerarius utero dixit. Lunae haerebat ipsaque monte succedere
+*Plurima vero potiuntur* igne. Orbe non auro, non in Ilithyiam umbra,
+**faciles**. Ripas auras iactu, novissima dum?
